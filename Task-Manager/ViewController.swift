16589c19446e460cee30a2e7e52d5e19d2e4edcc
Khalid Elatawy <khalidel-atawy@khalids-imac.local>
2019-05-26 17:27:38 +0200

    Initial Commit

---
+++ Task-Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task-Manager
+//
+//  Created by Khalid Saleh Elatawy on 5/26/19.
+//  Copyright © 2019 Khalid Saleh Elatawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
